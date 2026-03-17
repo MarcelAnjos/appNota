43c269618ab0db8b8217c4f250db05614360887a
MARCEL SANT ANA DOS ANJOS <47478118852@info.local>
2026-03-17 19:44:31 -0300

    isolando funcao

--- processamento.py
+++ processamento.py
@@ -1,20 +1,3 @@
-def calc_media(notas=[]):
-    media= sum(notas) / len(notas)
-    notas = notas.append(media)
-    if media <=0:
-        return print("ERRO: Valores incorretos!")
-    if media >10:
-        media = 10
-    return media
-
-def status(n):
-    if n >= 7:
-        n="APROVADO"
-        return n
-    else:
-        n="RECUPERACAO"
-        return n
-
 def validar_notas(notas):
     if not isinstance(notas, list):
         raise TypeError("As notas devem estar em uma lista")
